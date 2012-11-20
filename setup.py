bdcfe0a101284e5658429b89561fd6f98b80328a
derpston <derpston+git@sleepygeek.org>
2012-11-20 04:28:19 +0000

    Adding setup.py, for shoving this stuff into PyPI.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+      name = 'socketcache'
+   ,  version = '0.1.0'
+   ,  description = 'Maintains a cache of socket objects'
+   ,  author = 'Derp Ston'
+   ,  author_email = 'derpston+pypi@sleepygeek.org'
+   ,  packages = ['']
+   ,  package_dir = {'': 'src'}
+   )
+
